654138aea6ad99da7eb6605e8857b18350318095
lastsaintcrucified <lastsaintcrucified@gmail.com>
2023-02-28 22:56:51 +0600

    finished1

--- src/screens/chat/chat.screen.js
+++ src/screens/chat/chat.screen.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 
 export const ChatScreen = () => {
   return (
     <View>
+      <StatusBar />
       <Text> Chat Screen </Text>
     </View>
   );
--- src/screens/profile/profile.screen.js
+++ src/screens/profile/profile.screen.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Text, View, SafeAreaView } from "react-native";
+import { Text, View, SafeAreaView, StatusBar } from "react-native";
 
 export const ProfileScreen = () => {
   return (
     <SafeAreaView>
+      <StatusBar />
       <View>
         <Text> Profile Screen </Text>
       </View>
